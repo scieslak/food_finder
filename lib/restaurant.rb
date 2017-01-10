a6420209065dfd87ebb23b6c1b3206862a367c08
scieslak <scieslak@scieslak.com>
2017-01-10 11:55:45 +0000

    Write create_file and file_usable class methods.

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -6,10 +6,25 @@ class Restaurant
 
   def self.file_exists?
     # class should know if the reastaurant file exists
+    if @@filepath && File.exists?(@@filepath)
+      return true
+    else
+      return false
+    end
+  end
+
+  def self.file_usable?
+    return false unless @@filepath
+    return false unless File.exists?(@@filepath)
+    return false unless File.readable?(@@filepath)
+    return false unless File.writable?(@@filepath)
+    return true
   end
 
   def self.create_file
     # create the restaurant file
+    File.open(@@filepath, 'w') unless file_exists?
+    return file_usable?
   end
 
   def self.saved_restaurants
