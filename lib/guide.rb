0d0e3f7715928ee92d6574670632c04fc7e562a2
scieslak <scieslak@scieslak.com>
2017-01-11 14:42:11 +0000

    Add list restaurants functionality

--- lib/guide.rb
+++ lib/guide.rb
@@ -47,7 +47,7 @@ class Guide
   def do_action(action)
     case action
     when 'list'
-      puts'Listing...'
+      list
     when 'find'
       puts 'Finding...'
     when 'add'
@@ -57,18 +57,20 @@ class Guide
     else
       puts "\nI dont't understand that command.\n"
     end
+  end
 
+  def list
+    puts "\nListing restaurants\n\n".upcase
+    restaurants = Restaurant.saved_restaurants
+    restaurants.each do |rest|
+      puts rest.name + " | " + rest.cuisine + " | " + rest.price
+    end
   end
 
   def add
     puts "\nAdd a restaurant\n\n".upcase
-    restaurant = Restaurant.new
-    print "Restaurant name: "
-    restaurant.name = gets.chomp.strip
-    print "Cousine type: "
-    restaurant.cousine = gets.chomp.strip
-    print "Average price: "
-    restaurant.price = gets.chomp.strip
+
+    restaurant = Restaurant.build_using_questions
     if restaurant.save
       puts "\nRestaurant Added\n\n"
     else
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -4,7 +4,7 @@ class Restaurant
     @@filepath = File.join(APP_ROOT, path)
   end
 
-  attr_accessor :name, :cousine, :price
+  attr_accessor :name, :cuisine, :price
 
   def self.file_exists?
     # class should know if the reastaurant file exists
@@ -31,13 +31,48 @@ class Restaurant
 
   def self.saved_restaurants
     # read the restaurant file
+    restaurants = []
+    if file_usable?
+      file = File.new(@@filepath, 'r')
+      file.each_line do |line|
+        restaurants << Restaurant.new.import_line(line.chomp)
+      end
+      file.close
+    end
     # return instances of restaurant
+    return restaurants
+  end
+
+  def self.build_using_questions
+    args = {}
+    print "Restaurant name: "
+    args[:name] = gets.chomp.strip
+
+    print "Cousine type: "
+    args[:cuisine] = gets.chomp.strip
+
+    print "Average price: "
+    args[:price] = gets.chomp.strip
+
+    return self.new(args)
+  end
+
+  def initialize(args={})
+    @name = args[:name] || ""
+    @cuisine = args[:cuisine] || ""
+    @price = args[:price] || ""
+  end
+
+  def import_line(line)
+    line_array = line.split("\t")
+    @name, @cuisine, @price = line_array
+    return self
   end
 
   def save
     return false unless Restaurant.file_usable?
     File.open(@@filepath, 'a') do |file|
-      file.puts "#{[@name, @cousine, @price].join("\t")}\n"
+      file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
     end
     return true
   end
