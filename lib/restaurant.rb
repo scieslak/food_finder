f856e6e34333460956e40085f30a8840ffdf9d33
scieslak <scieslak@scieslak.com>
2017-01-11 13:51:22 +0000

    Add save restaurant functionality.

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -4,6 +4,8 @@ class Restaurant
     @@filepath = File.join(APP_ROOT, path)
   end
 
+  attr_accessor :name, :cousine, :price
+
   def self.file_exists?
     # class should know if the reastaurant file exists
     if @@filepath && File.exists?(@@filepath)
@@ -30,6 +32,14 @@ class Restaurant
   def self.saved_restaurants
     # read the restaurant file
     # return instances of restaurant
+  end
+
+  def save
+    return false unless Restaurant.file_usable?
+    File.open(@@filepath, 'a') do |file|
+      file.puts "#{[@name, @cousine, @price].join("\t")}\n"
+    end
+    return true
   end
 
 end
